a411c6c52b619e9e07a461b4239a2f4a40e8ec42
root <birukov_nickolai@mail.ru>
2016-11-20 01:28:25 +0300

    telegram bot implemented

--- bot/router/Controller.js
+++ bot/router/Controller.js
@@ -13,7 +13,6 @@ class Controller extends TelegramBaseController {
         if ($.message.location == null) return $.sendMessage(
             'Please send me your current location and I\'ll find something for you!');
         let location = $.message.location;
-        console.log(location);
         apiRequest('find', {
             coordinates: {
                 long: location.longitude,
@@ -24,9 +23,123 @@ class Controller extends TelegramBaseController {
                 console.log(err);
                 return $.sendMessage('Server error occured. Please try again later');
             }
-            console.log(result);
+            if (result.error) {
+                return $.sendMessage('Error: ' + result.error);
+            }
+            apiFindResponse($, result.result)
         });
     }
+}
+
+function apiFindResponse($, result) {
+    if (result.length == 0) return $.sendMessage("No retailers found");
+    let queue = [() => $.sendMessage('Following retailers found:')];
+    let menuLayout = [];
+    result.forEach(r => {
+        queue.push(()=> $.sendMessage('Company: ' + r.company)
+            .then(()=>$.sendLocation(r.coordinates.lat, r.coordinates.long)));
+        addMenuEntry($, menuLayout, r);
+    });
+    queue.push(() => $.runInlineMenu({
+        method: 'sendMessage',
+        params: ['Select company:'],
+        menu: menuLayout
+    }));
+    next(queue);
+}
+
+function addMenuEntry($, menu, entry) {
+    menu.push({
+        'text': entry.company,
+        'callback': ()=> {
+            retailerMenuSelect($, entry._id);
+        }
+    });
+}
+
+function retailerMenuSelect($, retailerID) {
+    apiRequest('getGoods', {
+        companyID: retailerID
+    }, (err, result) => {
+        if (err) {
+            console.log(err);
+            return $.sendMessage('Server error occured. Please try again later');
+        }
+        if (result.error) {
+            return $.sendMessage('Error: ' + result.error);
+        }
+        let order = {goods: []};
+        let menuLayout = [{
+            'text': 'reset',
+            'callback': () => resetOrder(order)
+        }, {
+            'text': 'order',
+            'callback': () => confirmOrder($, order)
+        }];
+        result.result.forEach(r => {
+            menuLayout.push({
+                'text': '₽' + r.price + ' ' + r.name + '(' + r.desc + ')',
+                'callback': () => addToOrder(order, r)
+            });
+        });
+        $.runInlineMenu({
+            method: 'sendMessage',
+            params: ['Make order:'],
+            menu: menuLayout
+        })
+    });
+}
+
+function resetOrder(order){
+    order.goods = [];
+}
+
+function confirmOrder($, order){
+    $.sendMessage('your order is:\n' +
+        order.goods.map(g => `₽${g.price} ${g.name} (${g.desc})`).join('\n')).then(()=>{
+        let menuLayout = [{
+            'text': 'YES',
+            'callback': () => performOrder($, order)
+        }, {
+            'text': 'NO',
+            'callback': () => {}
+        }];
+        $.runInlineMenu({
+            method: 'sendMessage',
+            params: ['Confirm?'],
+            layout: 2,
+            menu: menuLayout
+        })
+    });
+}
+
+function performOrder($, order) {
+    apiRequest('order', {
+        goods: order.goods
+    }, (err, result) => {
+        if (err) {
+            console.log(err);
+            return $.sendMessage('Server error occured. Please try again later');
+        }
+        if (result.error) {
+            return $.sendMessage('Error: ' + result.error);
+        }
+        $.sendMessage(`Your pin code is ${result.result}\nCome back again!`);
+    });
+}
+
+function addToOrder(order, good){
+    order.goods.push(good);
+}
+
+function next(queue) {
+    function _next() {
+        if (!queue.length) return;
+        let func = queue.shift();
+        if (!queue.length) func();
+        else func().then(_next);
+    }
+    _next();
 }
 
 function apiRequest(method, params, cb) {
